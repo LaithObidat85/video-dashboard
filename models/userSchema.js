eb9278ab0fe66de472416c18a1c33a8732aaa7dd
LaithObidat <laithobidat85@gmail.com>
2025-09-19 06:28:06 +0300

    Create userSchema.js

---
+++ models/userSchema.js
@@ -0,0 +1,13 @@
+// models/userSchema.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name:        { type: String, required: true, trim: true },
+  email:       { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password:    { type: String, required: true }, // مشفّرة بـ bcrypt
+  role:        { type: String, enum: ['admin', 'user'], default: 'user' },
+  isActive:    { type: Boolean, default: true },
+  createdAt:   { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
